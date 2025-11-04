2628a5910cd6f210fe6cd4ebf4cfe9c90c7fc2a8
don0104 <donaldlumio1@gmail.com>
2025-11-04 10:55:59 +0800

    third commit

--- app/config/config.php
+++ app/config/config.php
@@ -79,7 +79,7 @@ $config['ENVIRONMENT']             = 'development';
 | WARNING: You MUST set this value!
 |
 */
-$config['base_url'] 				= 'http://localhost/web2/final_project/car_rental/';
+$config['base_url'] 				= 'https://autotrack-wetd.onrender.com/';
 
 /*
 |--------------------------------------------------------------------------
